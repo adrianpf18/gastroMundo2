2a2fb48cda6ae0dd1a3bdcffda824e86e9dcdaa5
Adrianpf <adrianpalomofe@gmail.com>
2025-03-30 22:04:52 +0200

    Commit 3: Metodos setter y getter de la clase Alimento

--- Alimento.java
+++ Alimento.java
@@ -12,4 +12,27 @@ public class Alimento {
         this.tipo = tipo;
     }
     
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public int getCalorias() {
+        return calorias;
+    }
+    
+    public String getTipoAlimento() {
+        return tipo;
+    }
+    
+    public void setNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    
+    public void setCalorias(int nuevasCalorias) {
+        this.calorias = nuevasCalorias;
+    }
+    public void setTipo(String nuevoTipo) {
+        this.tipo = nuevoTipo;
+    }
+    
 }
